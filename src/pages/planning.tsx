d3bfba035b0e603b79ef8925649c63f019ffca05
ten5610 <ten_tokutomi@mac.com>
2023-03-20 16:58:23 +0900

    一画面で切り替えれるようにした

--- src/pages/planning.tsx
+++
@@ -1,9 +0,0 @@
-import { Planning } from '@/03_organisms/Planning'
-import { Main } from '@/04_templates/MainPanel';
-
-export default function Home() {
-  return <Main>
-    <h1>Planning</h1>
-    <Planning />
-  </Main>
-}
