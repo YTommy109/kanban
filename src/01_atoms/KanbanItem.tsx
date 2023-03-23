720e8bb119706419dd9055b4e40d11fb22d262e1
ten5610 <ten_tokutomi@mac.com>
2023-03-23 16:55:24 +0900

    feat: Done の時に消えるようにした。

--- src/01_atoms/KanbanItem.tsx
+++ src/01_atoms/KanbanItem.tsx
@@ -16,6 +16,18 @@ type Props = {
 export const KanbanItem: FC<Props> = ({ item, handleClick }) =>
   <Li key={item.id} className="item" onClick={handleClick}>{item.title}</Li>
 
+const STATE_VALUE = {
+  'ToDo': 'Start',
+  'Doing': 'Finish',
+  'Done': null
+}
+
+const STATE_CHANGE = {
+  'ToDo': { nextState: 'Doing', display: 'Start' },
+  'Doing': { nextState: 'Done', display: 'Finish' },
+  'Done': null
+}
+
 type Props2 = {
   item: BacklogItem;
   setFocusId?: (id: string) => void;
@@ -26,6 +38,6 @@ export const KanbanItem2: FC<Props2> = ({ item, setFocusId }) => {
 
   return <Li key={item.id} className="item">
     <span onClick={() => setFocusId ? setFocusId(item.id) : null}>{item.title}</span>
-    <span onClick={() => changeState(item.id, 'Doing')}>Doing</span>
+    {STATE_VALUE[item.state] && <span onClick={() => changeState(item.id, 'Done')}>{STATE_CHANGE[item.state]?.display}</span>}
   </Li>
 }
